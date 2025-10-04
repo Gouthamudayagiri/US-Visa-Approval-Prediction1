7d691f5e998c5316b493c88138bf5b6d8d52d75c
Gouthamudayagiri <gouthamudayagiri@gmail.com>
2025-10-04 15:52:28 +0530

    updated components

--- demo.py
+++ demo.py
@@ -1,14 +1,3 @@
-# from us_visa.logger import logging
-# # logging.info("Welcome to our logging")
-# from us_visa.exception import USvisaException
-# import sys
-
-# # try:
-# #     x = 1/0
-# # except Exception as e:
-# #    logging.error( USvisaException(e,sys))
-
-
 from us_visa.pipline.training_pipeline import TrainPipeline
 
 pipeline = TrainPipeline()
